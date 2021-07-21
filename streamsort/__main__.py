f165dc8c40039d505b3a6846dd282f6e08dca645
IdmFoundInHim <idmfoundinhim@gmail.com>
2021-07-21 17:35:10 -0400

    Create cache directory if needed when run

--- streamsort/__main__.py
+++ streamsort/__main__.py
@@ -1,5 +1,11 @@
+import os
 import sys
 
+from .constants import CACHE_DIR
+
 from .sh import shell
+
+if CACHE_DIR not in os.listdir():
+    os.makedirs(CACHE_DIR)
 
 sys.exit(shell())
--- streamsort/constants.py
+++ streamsort/constants.py
@@ -5,7 +5,8 @@ from spotipy import Spotify
 
 CLIENT_ID = "6400ca69c7dd4b969f2620d6d2647b03"
 REDIRECT_URI = "http://localhost:8080"
-CACHE_PATH = os.path.join('.cache', 'api.json')
+CACHE_DIR = '.cache'
+CACHE_PATH = os.path.join(CACHE_DIR, 'api.json')
 MOB_URI_PREFIX = 'spotify:'
 MOB_URL_PREFIX = 'https://open.spotify.com/'
 SPID_VALID_CHARS = string.ascii_letters + string.digits
