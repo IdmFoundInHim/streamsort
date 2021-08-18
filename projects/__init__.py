b2b02588a1acf1dc54463c09f4e60d8355a14d21
IdmFoundInHim <idmfoundinhim@gmail.com>
2021-08-18 13:45:46 -0400

    Add projects extension placeholder

---
+++ projects/__init__.py
@@ -0,0 +1,17 @@
+""" StreamSort Projects Extension
+
+Copyright (c) 2021 IdmFoundInHim, under MIT License
+
+Included with StreamSort as an example  of how to extend StreamSort.
+Notice that a `_sentences_` attribute is included in the top-level
+package. This is required for the StreamSort shell to properly import
+the extension.
+"""
+# pyright: reportUnsupportedDunderAll=false
+from .sentences import __all__ as sentences__all__
+
+__all__ = ['_sentences_'] + sentences__all__
+
+from .sentences import *
+
+_sentences_ = {'projects': proj_projects}
---
+++ projects/sentences.py
@@ -0,0 +1,11 @@
+""" StreamSort Projects Extension -- Sentences Module 
+
+Copyright (c) 2021 IdmFoundInHim, under MIT License
+"""
+__all__ = ['proj_projects']
+
+from streamsort.types import Query, State
+
+
+def proj_projects(subject: State, query: Query) -> State:
+    ...
